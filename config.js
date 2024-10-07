b645eabad4d4d16a1bfa554d56365491534b9530
Ashish <kodudula3@gmail.com>
2024-10-07 17:39:56 +0530

    Included auth

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const { GoogleAIFileManager } = require("@google/generative-ai/server");
 const multer = require('multer');
+const {mongoose} = require('mongoose');
 
 dotenv.config();
 
@@ -10,6 +11,12 @@ const geminiFileManager = new GoogleAIFileManager(process.env.GEMINI_API_KEY);
 const gemini = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const fileStorage = multer({dest:'uploads/'});
+
+const uri = 'mongodb+srv://rkodudula:D1Om6Sl0LS2DWzyD@cluster0.0wbzx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 module.exports.gemini = gemini;
 module.exports.geminiFileManager = geminiFileManager;
--- index.js
+++ index.js
@@ -1,53 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
-const {gemini, geminiFileManager, fileStorage} = require('./config');
+const { fileStorage } = require('./config');
+const { registerUserController, logInUserController, verifyToken } = require("./controllers/authControllers");
+const { chatWithGeminiController } = require("./controllers/chatControllers");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/chat", fileStorage.single('file'), async (req, res) => {
-  const finalQuery = {
-    history: req.body.history,
-    currentQuery: req.body.query
-  }
+app.post('/register', async (req, res) => {
+  registerUserController(req, res);
+})
 
-  const finalStringQuery = JSON.stringify(finalQuery);
-  let LLMResponse = '';
-  
-  try{
-    if(req.file){
-      const uploadResponse = await geminiFileManager.uploadFile(req.file.path, {
-        mimeType: "application/pdf",
-        displayName: "Gemini 1.5 PDF",
-      });
+app.post('/auth', async (req, res) => {
+  logInUserController(req, res);
+})
 
-      const result = await gemini.generateContent([
-        {
-          fileData: {
-            mimeType: uploadResponse.file.mimeType,
-            fileUri: uploadResponse.file.uri,
-          },
-        },
-        { text: finalStringQuery },
-      ]);
-
-      LLMResponse = result.response.text();
-    }
-    else {
-      LLMResponse = (await gemini.generateContent(finalStringQuery)).response.text();
-    }
-  }
-  catch (error){
-    LLMResponse = `Sorry, response couldn't be generated right now`;
-    console.error(error);
-  }
-
-  res.json({
-    role : 'Gemini', 
-    content : LLMResponse});
+app.post("/chat", verifyToken, fileStorage.single('file'), async (req, res) => {
+  chatWithGeminiController(req, res);
 });
 
 const PORT = process.env.PORT || 5000;
